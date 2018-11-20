01116d55c316195c796f874a485c01013a84855c
Joseph Goergen <jgoergen15@winona.edu>
2018-11-19 19:51:39 -0600

    added more stuff

--- src/main.java
+++ src/main.java
@@ -2,7 +2,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class main {
@@ -10,10 +15,25 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			ArrayList<ArrayList<Double>> table = new ArrayList<ArrayList<Double>>();
 			FileInputStream fis  = new FileInputStream(new File("data.xlsx"));
-			XSSFWorkbook sheet = new XSSFWorkbook(fis);
+			XSSFWorkbook workbook = new XSSFWorkbook(fis);
 			
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> rowIt = sheet.rowIterator();
 			
+			while(rowIt.hasNext())
+			{
+				Row row = rowIt.next();
+				Iterator<Cell> cellIterator = row.cellIterator();
+				ArrayList<Double> tempArrayList = new ArrayList<Double>();
+				while(cellIterator.hasNext())
+				{
+					Cell cell = cellIterator.next();
+					tempArrayList.add(Double.parseDouble(cell.toString() + ""));
+				}
+				table.add(tempArrayList);
+			}
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
