ba7207543f926a7c5a393c72663974f08ea2dd79
Joseph Goergen <jgoergen15@winona.edu>
2018-11-19 19:12:42 -0600

    excel stuff

--- src/main.java
+++ src/main.java
@@ -1,9 +1,24 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			FileInputStream fis  = new FileInputStream(new File("data.xlsx"));
+			XSSFWorkbook sheet = new XSSFWorkbook(fis);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
